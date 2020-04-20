ea67c31ac7024ef9178a5b33a42fcc372d0734fa
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-04-20 15:15:31 +0200

    Portfolio Object in place

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write(JSON.stringify(projects)); //Making whole object to STRING
+    response.end();
+
 });
 
 const port = process.env.PORT || 1337;
 server.listen(port);
+
+//My portffolio object
+const projects = [{
+    title: "Science Museum",
+    description: "In this Semester Project I projected and created a Website for a Science Museum new in the city easy to navigate and use, as well responsive.The site appeal primary fo middle school children’s between 7 to 15 years and encourages them,their families and school to visit the Science Museum.",
+    background_image: {
+        link: "http://www.upneto.com/htmlcss/sp01/index.html",
+        src: "images/science-museum.jpg",
+        alt: "Science Museum Website",
+        title: "Science Museum Semester Project",
+    },
+    technology: [{
+            name: "HTML5",
+            icon: "fab fa-html5 fa-2x"
+        },
+        {
+            name: "CSS3",
+            icon: "fab fa-css3-alt fa-2x"
+        },
+        {
+            name: "ADOBE",
+            icon: "fab fa-adobe fa-2x"
+        }
+    ]
+}];
+
+
+
 
 console.log("Server running at http://localhost:%d", port);
